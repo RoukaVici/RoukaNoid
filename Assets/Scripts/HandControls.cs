2f59720efb14e6fe8a89088c870f2366ac7e39e2
Unknown <louis.jahn@epitech.eu>
2018-07-21 23:30:13 +0200

    Player can grab objects and drop them

--- Assets/Scripts/HandControls.cs
+++ Assets/Scripts/HandControls.cs
@@ -4,28 +4,32 @@ using UnityEngine;
 
 public class HandControls : MonoBehaviour {
 
-	[SerializeField]
-	float speed = 1f;
-	[SerializeField]
-	private Vector3 positiveOffsetLimit, negativeOffsetLimit;
+	[SerializeField] float speed = 1f;
+	[SerializeField] private Vector3 positiveOffsetLimit, negativeOffsetLimit;
 
+	[SerializeField] private float lerpMovement = 0.5f;
+
+	[SerializeField]
+	private Animator anim;
 	private Vector3 originalPosition;
 	private Vector3 targetPosition;
+	private bool isGrabbing = false;
+	private GameObject grabbedObject;
+	private Transform grabbedObjectParent;
 
 	void Start ()
 	{
 		originalPosition = transform.position;
 		targetPosition = originalPosition;
 	}
-	
-	public float testLerp = 0.5f;
-	void FixedUpdate ()
+
+	void MoveHands()
 	{
 		float x = Input.GetAxis("Mouse X") * Time.deltaTime * speed;
 		float y = 0;
 		float z = Input.GetAxis("Mouse Y") * Time.deltaTime * speed;
 
-		if (Input.GetAxis("Fire1") == 1)
+		if (Input.GetAxis("Fire2") == 1)
 		{
 			y = z;
 			z = 0;
@@ -39,7 +43,49 @@ public class HandControls : MonoBehaviour {
 		targetPosition.y = Mathf.Clamp(targetPosition.y, originalPosition.y - negativeOffsetLimit.y, originalPosition.y + positiveOffsetLimit.y);
 		targetPosition.z = Mathf.Clamp(targetPosition.z, originalPosition.z - negativeOffsetLimit.z, originalPosition.z + positiveOffsetLimit.z);
 
-		transform.position = Vector3.Lerp(transform.position, targetPosition, testLerp);
+		transform.position = Vector3.Lerp(transform.position, targetPosition, lerpMovement);
+	}
+
+	void AnimateHand()
+	{
+		if (Input.GetAxis("Fire1") > 0)
+			anim.SetBool("isGrabbing", true);
+		else
+		{
+			anim.SetBool("isGrabbing", false);
+			isGrabbing = false;
+			if (grabbedObject)
+			{
+				grabbedObject.GetComponent<Rigidbody>().isKinematic = false;
+				grabbedObject.transform.SetParent(grabbedObjectParent);
+				grabbedObject = null;
+			}
+		}
+	}
+
+	void GrabObject()
+	{
+
+	}
+	
+	void FixedUpdate ()
+	{
+		MoveHands();
+		AnimateHand();
+		GrabObject();
+	}
+
+	void OnCollisionEnter(Collision other)
+	{
+		if (!isGrabbing && other.gameObject.tag == "Grabable"
+		&& anim.GetCurrentAnimatorStateInfo(0).IsName("Grab") && anim.GetCurrentAnimatorStateInfo(0).normalizedTime >= 1)
+		{
+			other.gameObject.GetComponent<Rigidbody>().isKinematic = true;
+			grabbedObjectParent = other.transform.parent;
+			grabbedObject = other.gameObject;
+			other.transform.SetParent(transform);
+			isGrabbing = true;
+		}
 	}
 
 }
---
+++ Assets/Scripts/SkinnedMeshCollider.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(MeshCollider))]
+public class SkinnedMeshCollider : MonoBehaviour {
+	[SerializeField]
+   	private SkinnedMeshRenderer meshRenderer;
+    private MeshCollider mCollider;
+
+	void Start()
+	{
+		mCollider = GetComponent<MeshCollider>();
+	}
+	
+	void Update ()
+	{
+		Mesh colliderMesh = new Mesh();
+		meshRenderer.BakeMesh(colliderMesh);
+        mCollider.sharedMesh = null;
+        mCollider.sharedMesh = colliderMesh;
+	}
+}
